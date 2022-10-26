dd93e1ce35cfe8087397656f0857cb3d12becdeb
josh4500 <jibo.ajosh45@gmail.com>
2022-10-26 12:23:40 +0100

    Added error handling page

--- dist/index.js
+++ dist/index.js
@@ -3,14 +3,40 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const http_errors_1 = __importDefault(require("http-errors"));
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./src/config/db"));
+const path_1 = __importDefault(require("path"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+app.use((_, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 //Connection to the DB
 (0, db_1.default)();
+// View engine setup.
+app.set("views", path_1.default.join(__dirname, "..", "views"));
+app.set("view engine", "ejs");
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.static(path_1.default.join(__dirname, "..", "public")));
+app.use(function (req, res, next) {
+    console.log(req.url);
+    next((0, http_errors_1.default)(404));
+});
+app.use(function (err, req, res, _next) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    res.status(err.status || 500);
+    res.render("error");
+});
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server');
 });
--- index.ts
+++ index.ts
@@ -1,14 +1,54 @@
+import createError from "http-errors";
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import connectDB from './src/config/db';
+import path from 'path';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use((_, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
 //Connection to the DB
 connectDB();
+
+// View engine setup.
+app.set("views", path.join(__dirname, "..", "views"));
+app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.use(function (req, res, next) {
+    console.log(req.url);
+    next(createError(404));
+});
+
+app.use(function (
+    err: createError.HttpError,
+    req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction
+) {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    res.status(err.status || 500);
+    res.render("error");
+});
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
